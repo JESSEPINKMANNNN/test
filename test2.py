e676b9dec9b6d672e7992569638c254015116c14
JESSEPINKMANNNN <f2023266440@umt.edu.pk>
2025-08-24 18:30:13 +0500

    testing feature branch

---
+++ test2.py
@@ -0,0 +1,8 @@
+#inlcude iostream
+using namespace std;
+int main;
+{
+cout << "you told me that i was crazy till i pulled up on your man and you. you looked like a devil when you waved at me. bitch i got something to say to you, GOODBYE!":
+}
+
+endl;
